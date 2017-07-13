1af71b0e266403438d26779de2bbb780b1a0e887
Nicholas Beach <nbeach@pillartechnology.com>
2017-07-13 16:40:44 -0400

    Added auto-refresh server to build watcher

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 - Node.js v6.9.1 or higher
 
 ### Building the project
-To build the project you first need to install the needed Node packages by executing the command `npm install` from the project directory.
+To build the project you first need to install the needed Node packages by executing the command `npm install` from the 
+project directory.
 
-You can then build the project using `npm run build`. The built files are output to /dist
+You can then build the project using `npm run build`. The built files are output to `dist/`
+
+You can also build the project using `npm run build:watch` which will watch the source files and re-build the project 
+whenever a source file changes. It also launches a development server on port 8080 that automatically refreshes the 
+browser when a file is changed. This allows for a fast feedback cycle and rapid iteration.
+
+###Notes
+TODO: Talk about minimizing payload
--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,54 @@
 const gulp = require('gulp'),
     del = require('del'),
     rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
     less = require('gulp-less'),
-    uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin'),
     sourcemaps = require('gulp-sourcemaps'),
     pug = require('gulp-pug'),
-    _ = require("lodash"),
     join = require('path').join;
 
 const outputPath = "dist";
 const sourcePath = "src";
 
-const tasks = [
-    {
-        name: "clean",
-        path: join(outputPath, "*"),
-        steps: path => () => del(path)
-    },
-    {
-        name: "template",
-        watch: join(sourcePath, "**/*"),
-        path: join(sourcePath, "index.pug"),
-        steps: path => () => gulp.src(path)
-            .pipe(pug({}))
-            .pipe(gulp.dest(outputPath))
-    },
-    {
-        name: "style",
-        path: join(sourcePath, "style/style.less"),
-        steps: path => () => gulp.src(path)
-            .pipe(less())
-            .pipe(rename('style.css'))
-            .pipe(gulp.dest(outputPath))
-            .pipe(rename('style.min.css'))
-            .pipe(sourcemaps.init())
-            .pipe(cssmin())
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(outputPath))
-    },
-    {
-        name: "image",
-        path: [join(sourcePath, 'image/**/*')],
-        steps: path => () => gulp.src(path)
-            .pipe(gulp.dest(join(outputPath, '/image')))
-    },
-    {
-        name: "font",
-        path: [join(sourcePath, 'font/**/*'), 'node_modules/bootstrap/dist/fonts/**/*'],
-        steps: path => () => gulp.src(path)
-            .pipe(gulp.dest(join(outputPath, 'font')))
-    },
-];
 
-tasks.forEach(task => gulp.task(task.name, task.steps(task.path)));
+gulp.task("clean", () => del(join(outputPath, "*")));
 
-gulp.task('watch', () => tasks
-    .filter(task => task.name !== "clean")
-    .forEach(task => gulp.watch(task.watch ? task.watch : task.path, [task.name])));
+gulp.task("template", () => gulp
+    .src(join(sourcePath, "index.pug"))
+    .pipe(pug({}))
+    .pipe(gulp.dest(outputPath))
+);
 
-gulp.task('default', _.chain(tasks)
-    .map(task => task.name)
-    .filter(name => name !== "clean")
-    .value());
+gulp.task("style", () => gulp
+    .src(join(sourcePath, "style/style.less"))
+    .pipe(less())
+    .pipe(rename("style.css"))
+    .pipe(gulp.dest(outputPath))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemaps.init())
+    .pipe(cssmin())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(outputPath))
+);
+
+gulp.task("image", () => gulp
+    .src(join(sourcePath, "image/**/*"))
+    .pipe(gulp.dest(join(outputPath, "/image")))
+);
+
+gulp.task("font", () => gulp
+    .src([join(sourcePath, "font/**/*"), "node_modules/bootstrap/dist/fonts/**/*"])
+    .pipe(gulp.dest(join(outputPath, "font")))
+);
 
 
+gulp.task("default", ["template", "style", "image", "font"]);
 
-
+gulp.task('watch', ['default'], () => {
+    gulp.watch(join(sourcePath, "**/*.pug"),   ["template"]);
+    gulp.watch(join(sourcePath, "**/*.less"),  ["style"]);
+    gulp.watch(join(sourcePath, "image/**/*"), ["image"]);
+    gulp.watch(join(sourcePath, "font/**/*"),  ["font"]);
+});
 
 
